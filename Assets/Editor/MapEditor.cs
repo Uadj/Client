f91262d1b9a6f37dea8e0772e6a0b5bd138fee4c
dgw <ds257873@naver.com>
2022-10-25 16:07:48 +0900

    MapEditor

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿	using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -13,6 +13,45 @@ public class MapEditor
 
 #if UNITY_EDITOR
 
+	[MenuItem("Tools/GenerateMap2 %#g")]
+	private static void Hello()
+    {
+		GameObject go = GameObject.Find("Map");
+        if (go == null)
+        {
+			return;
+        }
+		Tilemap tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
+		if (tm == null)
+		{
+			return;
+		}
+		
+		//txt
+		using(var writer = File.CreateText("Assets/Resources/Map/output.txt"))
+        {
+			writer.WriteLine(tm.cellBounds.xMin);
+			writer.WriteLine(tm.cellBounds.xMax);
+			writer.WriteLine(tm.cellBounds.yMin);
+			writer.WriteLine(tm.cellBounds.yMax);
+
+			int count = 0;
+			for(int y=tm.cellBounds.yMax; y >= tm.cellBounds.yMin; y--)
+            {	
+				for (int x = tm.cellBounds.xMin; x <= tm.cellBounds.xMax; x++)
+                {
+					count++;
+					if (count > 1000) return;
+					TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
+					if (tile != null)
+						writer.Write("1");
+					else
+						writer.Write("0");
+                }
+				writer.WriteLine();
+			}
+		}
+	}
 	// % (Ctrl), # (Shift), & (Alt)
 
 	[MenuItem("Tools/GenerateMap %#g")]
@@ -42,6 +81,7 @@ public class MapEditor
 				{
 					for (int x = tmBase.cellBounds.xMin; x <= tmBase.cellBounds.xMax; x++)
 					{
+						
 						TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
 						if (tile != null)
 							writer.Write("1");
